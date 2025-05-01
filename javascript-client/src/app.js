b4cf1bf1071e51b2a3fafdab38a3a7eeebe2171b
Alice Jackson <129292543+palacejackson@users.noreply.github.com>
2025-05-01 14:46:49 +0100

    Update app.js to JS V5
    CodeSandbox was not functioning as syntax required to function with JS client V5 needed updating

--- javascript-client/src/app.js
+++ javascript-client/src/app.js
@@ -1,19 +1,28 @@
-import algoliasearch from 'algoliasearch';
+import { algoliasearch } from 'algoliasearch';
 
 const client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
-const index = client.initIndex('instant_search');
 
 const $searchBox = document.querySelector('#searchBox input[type=search]');
 const $hits = document.querySelector('#hits');
 
 function renderHits(query) {
-  index.search(query).then(result => {
-    // Please sanitize user-provided data when using `innerHTML` to avoid XSS
-    $hits.innerHTML = `
+  client
+    .search({
+      requests: [
+        {
+          indexName: 'instant_search',
+          query: query,
+        },
+      ],
+    })
+    .then(({ results }) => {
+      const hits = results[0].hits || [];
+      // Please sanitize user-provided data when using `innerHTML` to avoid XSS
+      $hits.innerHTML = `
       <ol class="ais-hits">
-        ${result.hits
+        ${hits
           .map(
-            hit =>
+            (hit) =>
               `<li class="ais-hits--item">
                 <article>
                   <div>
@@ -25,13 +34,14 @@ function renderHits(query) {
           )
           .join('')}
       </ol>`;
-  });
+    });
 }
 
-$searchBox.addEventListener('input', event => {
+$searchBox.addEventListener('input', (event) => {
   const query = event.target.value;
 
   renderHits(query);
 });
 
 renderHits('');
+
